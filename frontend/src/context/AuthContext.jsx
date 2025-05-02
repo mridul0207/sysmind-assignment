07cc45f0d15c8cf7df0c9c773492a37e2cb6ce70
unknown <guptamridul0207@gmail.com>
2025-05-02 18:24:33 +0530

    Completed

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -38,7 +38,7 @@ export const AuthProvider = ({ children }) => {
     try {
       setLoading(true);
       setError(null);
-      const { data } = await axios.post('https://sysmind-assignment-backend.vercel.app/api/users/login', {
+      const { data } = await axios.post('http://localhost:5000/api/users/login', {
         email,
         password,
       });
@@ -56,7 +56,7 @@ export const AuthProvider = ({ children }) => {
     try {
       setLoading(true);
       setError(null);
-      const { data } = await axios.post('https://sysmind-assignment-backend.vercel.app/api/users', {
+      const { data } = await axios.post('http://localhost:5000/api/users', {
         username,
         email,
         password,
@@ -75,7 +75,7 @@ export const AuthProvider = ({ children }) => {
     try {
       setLoading(true);
       if (user && user._id) {
-        await axios.post('https://sysmind-assignment-backend.vercel.app/api/users/logout');
+        await axios.post('http://localhost:5000/api/users/logout');
       }
       setUser(null);
       localStorage.removeItem('user');
